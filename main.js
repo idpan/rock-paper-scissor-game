1adc16538d7404edd3d74a78951c0a4d2ca40692
idpan <idpanashari813@gmail.com>
2022-04-01 15:24:09 +0700

    menambah interaktifitas

--- main.js
+++ main.js
@@ -1,20 +1,20 @@
 let player;
 let house;
+let winStatus;
 let score = 0;
 
 const rulesBtn = document.querySelector(".rules-btn");
 const closeRule = document.querySelector(".close");
 const rulesBack = document.querySelector(".rules-back");
-const winStatus = document.querySelector(".win-status").children[0];
-const rock = document.querySelector('[data-item="rock"]');
-const paper = document.querySelector('[data-item="paper"]');
-const scissor = document.querySelector('[data-item="scissor"]');
+const winDeclare = document.querySelector(".win-status");
 const chooseArea = document.querySelector(".choose-area");
 const inGameArea = document.querySelector(".in-game-area");
-const playerPicked = document.querySelector(".player-picked").children[1];
-const housePicked = document.querySelector(".house-picked").children[1];
+const playerPicked = document.querySelector(".player-picked");
+const housePicked = document.querySelector(".house-picked");
 const playAgainBtn = document.querySelector(".play-again");
 const scoreDisplay = document.querySelector(".score-count");
+const chooseElement = document.querySelector(".choose").querySelectorAll(".outer");
+
 function pickedForPlayer(param) {
   return param.dataset.item;
 }
@@ -39,61 +39,78 @@ function winnerChooser(resultForPlayer, resultForHouse) {
     if (resultForHouse === "paper") return "win";
   }
 }
+function displayToNone(domElement) {
+  domElement.style.display = "none";
+}
+function displayToFlex(domElement) {
+  domElement.style.display = "flex";
+}
+function displayToBlock(domElement) {
+  domElement.style.display = "block";
+}
+function displaySwitcher(resultForPlayer, resultForHouse, winStatus) {
+  displayToNone(chooseArea);
+  displayToFlex(inGameArea);
+  setTimeout(displayToFlex, 900, housePicked.querySelector(".circle"));
+  playerPicked.querySelector(".circle").classList.add(resultForPlayer);
+  spin();
+  setTimeout(function () {
+    housePicked.querySelector(".circle").classList.add(resultForHouse);
+  }, 2000);
+  // setTimeout(displayToFlex, , housePicked.querySelector(".circle"));
+  setTimeout(displayToBlock, 2400, winDeclare);
 
-function displaySwitcher(resultForPlayer, resultForHouse) {
-  chooseArea.style.display = "none";
-  inGameArea.style.display = "flex";
-  playerPicked.classList.add(resultForPlayer);
-  housePicked.classList.add(resultForHouse);
+  if (winStatus === "win") {
+    setTimeout(displayToFlex, 2500, playerPicked.querySelector(".winner-shadow"));
+  }
+  if (winStatus === "lose") {
+    setTimeout(displayToFlex, 2500, housePicked.querySelector(".winner-shadow"));
+  }
 }
 function winStatusDisplay(resultWinnerChosser) {
-  if (resultWinnerChosser === "win") return (winStatus.textContent = "you win");
-  if (resultWinnerChosser === "lose") return (winStatus.textContent = "you lose");
-  return (winStatus.textContent = "draw");
+  if (resultWinnerChosser === "win") return (winDeclare.children[0].textContent = "you win");
+  if (resultWinnerChosser === "lose") return (winDeclare.children[0].textContent = "you lose");
+  return (winDeclare.children[0].textContent = "draw");
 }
 function playAgain() {
-  playerPicked.classList.remove(player);
-  housePicked.classList.remove(house);
-  chooseArea.style.display = "flex";
-  inGameArea.style.display = "none";
+  playerPicked.querySelector(".circle").classList.remove(player);
+  housePicked.querySelector(".circle").classList.remove(house);
+  displayToNone(inGameArea);
+  displayToFlex(chooseArea);
+  displayToNone(housePicked.querySelector(".circle"));
+  displayToNone(playerPicked.querySelector(".winner-shadow"));
+  displayToNone(housePicked.querySelector(".winner-shadow"));
+  displayToNone(winDeclare);
+}
+function spin() {
+  const element = ["rock", "paper", "scissor"];
+  let i = 0;
+  const roller = setInterval(function () {
+    housePicked.querySelector(".circle").classList.remove(element[element.length - 1]);
+
+    housePicked.querySelector(".circle").classList.remove(element[i - 1]);
+    housePicked.querySelector(".circle").classList.add(element[i++]);
+    if (i === element.length) i = 0;
+  }, 100);
+  setTimeout(clearInterval, 2000, roller);
 }
 
-// playerPicked click
-rock.onclick = function () {
-  player = pickedForPlayer(this);
-  house = pickedForHouse();
-  winStatusDisplay(winnerChooser(player, house));
-  displaySwitcher(player, house);
-  if (winnerChooser(player, house) === "win") {
-    score += 1;
+chooseElement.forEach(function (element) {
+  element.onclick = function () {
+    player = pickedForPlayer(this);
+    house = pickedForHouse();
+    winStatus = winnerChooser(player, house);
+    winStatusDisplay(winStatus);
+    displaySwitcher(player, house, winStatus);
+    if (winStatus === "win") {
+      score++;
+      setTimeout(() => {
+        scoreDisplay.textContent = score;
+      }, 2400);
+    }
+  };
+});
 
-    scoreDisplay.textContent = score;
-  }
-};
-paper.onclick = function () {
-  player = pickedForPlayer(this);
-  house = pickedForHouse();
-  displaySwitcher(player, house);
-  winStatusDisplay(winnerChooser(player, house));
-
-  if (winnerChooser(player, house) === "win") {
-    score += 1;
-
-    scoreDisplay.textContent = score;
-  }
-};
-scissor.onclick = function () {
-  player = pickedForPlayer(this);
-  house = pickedForHouse();
-  displaySwitcher(player, house);
-  winStatusDisplay(winnerChooser(player, house));
-
-  if (winnerChooser(player, house) === "win") {
-    score += 1;
-
-    scoreDisplay.textContent = score;
-  }
-};
 // play again click
 playAgainBtn.onclick = function () {
   playAgain();
